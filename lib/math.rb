a98454bc12d8b3d352fff1f418342bfd498bc33b
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-12 15:42:07 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  dividend = num1 / num2
+  puts dividend
+  dividend
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
+  remainder
 end
 
 def square_root(num)
-
+  square = Math.sqrt(num)
+  puts square
+  square
 end
